953c0481e284c09632158712a015616b05f70ef4
EmineGursoy <esgursoy@outlook.com>
2022-06-08 23:10:11 +0300

    tab bar added

---
+++ CryptocurrencyApp/CryptocurrencyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptocurrencyApp
+//
+//  Created by Emine Sinem Gursoy on 08.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
