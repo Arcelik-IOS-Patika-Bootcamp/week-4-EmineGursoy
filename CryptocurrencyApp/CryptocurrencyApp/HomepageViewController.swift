dc9578e3d64bb1f6c377112dd332a2bf963cb858
EmineGursoy <esgursoy@outlook.com>
2022-06-09 19:24:35 +0300

    search bar added

--- CryptocurrencyApp/CryptocurrencyApp/HomepageViewController.swift
+++ CryptocurrencyApp/CryptocurrencyApp/HomepageViewController.swift
@@ -9,21 +9,20 @@ import UIKit
 
 class HomepageViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setUpUI(){
+        searchBar.delegate = self
+        searchBar.placeholder = "Search"
     }
-    */
+}
 
+extension HomepageViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+    }
 }
